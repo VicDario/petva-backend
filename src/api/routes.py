142ae9dce539a96e8fc1652dd9c5e45d238816fd
VicDario <26585356+VicDario@users.noreply.github.com>
2021-07-29 17:42:04 -0400

    Added route for check

--- src/api/routes.py
+++ src/api/routes.py
@@ -794,12 +794,17 @@ def add_surgery_foundation_to_pet(pet_id):
 
     return jsonify(Success="Surgery added in history pet id: {}".format(pet.id)), 201
 
-@api.route('pets/in_adoption/<int:page>', methods=['GET'])
+@api.route('/pets/in_adoption/<int:page>', methods=['GET'])
 def get_pets_in_adoption(page=1):
     pets = Pet.query.filter_by(state=Pet_state.adoption).paginate(page=page, per_page=10)
     return jsonify([pet.serialize_info_for_adoption() for pet in pets.items], pets.pages, pets.has_next, pets.has_prev), 200
 
-@api.route('pets/lost/<int:page>', methods=['GET'])
+@api.route('/pets/lost/<int:page>', methods=['GET'])
 def get_pets_lost(page=1):
     pets = Pet.query.filter_by(state=Pet_state.lost).paginate(page=page, per_page=10)
     return jsonify([pet.serialize_info_for_lost() for pet in pets.items], pets.pages, pets.has_next, pets.has_prev), 200
+
+@api.route('/check', methods=['GET'])
+@jwt_required()
+def check_key():
+    return jsonify(Success="Checked"), 200
